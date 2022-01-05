f2f0102c808e02f533e0cd76a92c21f622ca67c0
charlesjin123 <71144512+charlesjin123@users.noreply.github.com>
2022-01-05 14:18:04 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/generate/:appName', (req, res) => {
+	generate_app(req.params.appName, () => {
+		//res.send("App generated with appName: " + req.params.appName);
+		const file = __dirname + "/public/" + req.params.appName + ".zip";
+		res.download(file);
+	});
+	
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.use(express.static('public'))
+
+function generate_app(appName, callback) {
+	const exec = require("child_process").exec;
+	exec(
+		"cd ../test-yo/generator-test" + " && " +
+		"yo test " + appName + " --force" + " && " +
+		"powershell Compress-Archive public/" + appName + " ../../test-expressjs/public/" + appName + ".zip"
+		, (error, stdout, stderr) => {
+	    if (error) {
+	        console.log(`error: ${error.message}`);
+	        return;
+	    }
+	    // if (stderr) {
+	    //     console.log(`stderr: ${stderr}`);
+	    //     return;
+	    // }
+	    console.log(`stdout: ${stdout}`);
+	    callback();
+	});
+}
